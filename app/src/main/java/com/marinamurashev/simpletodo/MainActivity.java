96f77efa55f7ed845e77209242d4204f7ff3d2b0
Marina Murashev <marinamurashev@gmail.com>
2014-12-09 19:52:53 -0800

    adds edit item activity

--- app/src/main/java/com/marinamurashev/simpletodo/MainActivity.java
+++ app/src/main/java/com/marinamurashev/simpletodo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.marinamurashev.simpletodo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -79,6 +80,17 @@ public class MainActivity extends Activity {
                     return true;
                 }
             }
+        );
+
+        lvItems.setOnItemClickListener(
+                new AdapterView.OnItemClickListener(){
+                    @Override
+                    public void onItemClick(AdapterView<?> adapter,
+                                            View item, int position, long id){
+                        Intent intent = new Intent(MainActivity.this, EditItemActivity.class);
+                        startActivity(intent);
+                    }
+                }
         );
     }
 
