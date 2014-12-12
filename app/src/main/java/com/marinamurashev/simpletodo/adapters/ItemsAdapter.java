7b3f6ef8fec9d4bd928cd5ef8ce09225a5dd2806
Marina Murashev <marinamurashev@gmail.com>
2014-12-12 15:38:39 -0800

    speeds up the itemsadapter with a view holder

--- app/src/main/java/com/marinamurashev/simpletodo/adapters/ItemsAdapter.java
+++ app/src/main/java/com/marinamurashev/simpletodo/adapters/ItemsAdapter.java
@@ -14,6 +14,10 @@ import java.util.ArrayList;
 
 public class ItemsAdapter extends ArrayAdapter<Item>{
 
+    private static class ViewHolder {
+        TextView name;
+    }
+
     public ItemsAdapter(Context context, ArrayList<Item> items) {
         super(context, 0, items);
     }
@@ -21,11 +25,20 @@ public class ItemsAdapter extends ArrayAdapter<Item>{
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         Item item = getItem(position);
+
+        ViewHolder viewHolder;
         if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item, parent, false);
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.item, parent, false);
+            viewHolder.name = (TextView) convertView.findViewById(R.id.tvName);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
         }
-        TextView tvName = (TextView) convertView.findViewById(R.id.tvName);
-        tvName.setText(item.getName());
+
+        viewHolder.name.setText(item.getName());
+
         return convertView;
     }
 
