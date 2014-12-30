5318f42c0e5a9366ba6dd289d0cd25879664e9a1
Marina Murashev <marinamurashev@gmail.com>
2014-12-29 18:12:14 -0800

    shows date dialog when edit item's date button is pressed

--- app/src/main/java/com/marinamurashev/simpletodo/fragments/DatePickerFragment.java
+++ app/src/main/java/com/marinamurashev/simpletodo/fragments/DatePickerFragment.java
@@ -4,12 +4,21 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.view.View;
+
+import com.marinamurashev.simpletodo.R;
+
 
 public class DatePickerFragment extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState){
+
+        View v = getActivity().getLayoutInflater()
+                .inflate(R.layout.dialog_date, null);
+
         return new AlertDialog.Builder(getActivity())
+                .setView(v)
                 .setPositiveButton(android.R.string.ok, null)
                 .create();
     }
